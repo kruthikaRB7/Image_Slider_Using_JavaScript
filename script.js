b69569a5128def1755072c2736ed54d2961e374c
kruthikaRB7 <kruthikarb7@gmail.com>
2025-04-26 11:59:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto slide (optional)
+// setInterval(nextSlide, 3000); // Change every 3 seconds
